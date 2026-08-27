1b22f306c9c8e2f24824815801704876376bfc2d
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-08-27 17:33:51 +0900

    feat: CraftComponent에 실제 아이템 제작 시 불러올 함수 생성
    InventoryComponent를 참고해야하는 함수다 보니 내부에 로직만 적어둠.

--- ProjectSR/Source/ProjectSR/Public/MainPanel/CraftComponent.h
+++ ProjectSR/Source/ProjectSR/Public/MainPanel/CraftComponent.h
@@ -6,6 +6,10 @@
 #include "Components/ActorComponent.h"
 #include "CraftComponent.generated.h"
 
+DECLARE_DYNAMIC_DELEGATE(FOnItemCraftDataAssetChange);
+
+class UInventoryComponent;
+class UItemCraftDataAsset;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PROJECTSR_API UCraftComponent : public UActorComponent
@@ -24,5 +28,16 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	// InCraftBlueprint의 재료 아이템을 InSourceInventory에서 탐색, 이후 존재하면 결과 아이템을 OutTargetInventory에 추가하는 함수.
+	void	CraftItem(UItemCraftDataAsset* InItemCraftDataAsset, UInventoryComponent* InSourceInventory, UInventoryComponent* OutTargetInventory);
+
+	//void	
+
+	FOnItemCraftDataAssetChange	OnItemCraftDataAssetChange;
+
+protected:
+	//UPROPERTY(Blue)
+	// 현재 가지고 있는 아이템 재작 설계도 목록
+	TArray<UItemCraftDataAsset*>	ItemCraftDataAssetList_;
+
 };
