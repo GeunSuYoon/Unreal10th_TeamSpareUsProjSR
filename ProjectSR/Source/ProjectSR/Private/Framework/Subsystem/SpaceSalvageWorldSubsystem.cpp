d9ff853d98b3d58625ed61a0dc17f54487ac5cbf
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-08-28 17:43:31 +0900

    feat: 우주 맵 서브시스템 생성 및 아이템 스폰 로직 추가
    아직 버그때문에 제대로 스폰되지는 않음.

---
+++ ProjectSR/Source/ProjectSR/Private/Framework/Subsystem/SpaceSalvageWorldSubsystem.cpp
@@ -0,0 +1,227 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Framework/Subsystem/SpaceSalvageWorldSubsystem.h"
+#include "Framework/Subsystem/ItemActorFactorySubsystem.h"
+#include "RootActor/SpaceRootActor.h"
+#include "SpaceShip/SpaceShipActor.h"
+#include "SpaceShip/MeteorAvoidanceComponent.h"
+#include "Data/SpaceMap/SpaceMapItemSpawnRateDataAsset.h"
+#include "Item/ItemActor.h"
+
+bool USpaceSalvageWorldSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	Super::ShouldCreateSubsystem(Outer);
+	const UWorld*	World = Cast<UWorld>(Outer);
+
+	UE_LOG(LogTemp, Warning, TEXT("SpaceSalvageWorldSubsystem 실행"));
+
+	return (IsValid(World) && World->IsGameWorld());
+}
+
+void USpaceSalvageWorldSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("SpaceSalvageSubsystem Created | World: %s | Type: %d | NetMode: %d"),
+		*GetWorld()->GetName(),
+		static_cast<int32>(GetWorld()->WorldType),
+		static_cast<int32>(GetWorld()->GetNetMode())
+	);
+}
+
+void USpaceSalvageWorldSubsystem::OnWorldBeginPlay(UWorld& InWorld)
+{
+	Super::OnWorldBeginPlay(InWorld);
+
+	this->SpawnSpaceRoot__();
+}
+
+void USpaceSalvageWorldSubsystem::Deinitialize()
+{
+
+	Super::Deinitialize();
+}
+
+void USpaceSalvageWorldSubsystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void USpaceSalvageWorldSubsystem::SetSpaceMapData(USpaceMapItemSpawnRateDataAsset* InItemRateData)
+{
+	if (!InItemRateData)
+	{
+		return ;
+	}
+	this->ItemSpawnRateData__ = InItemRateData;
+	this->ItemSpawnTimer__ = InItemRateData->ItemSpawnTime;
+	this->ItemSpawnDist__ = InItemRateData->ItemSpawnDist;
+	this->ItemMoveSpeed__ = InItemRateData->ItemMoveSpeed;
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	TimerManager.SetTimer(
+		this->ItemSpawnHandler__,
+		this,
+		&USpaceSalvageWorldSubsystem::SpawnItemActor__,
+		1.0f,
+		true,
+		this->ItemSpawnTimer__
+	);
+	UE_LOG(LogTemp, Log, TEXT("MapData %s가 할당됐습니다."), *InItemRateData->MapName.ToString());
+}
+
+void USpaceSalvageWorldSubsystem::RegisterSpaceShipActor(ASpaceShipActor* InSpaceShip)
+{
+	if (!IsValid(InSpaceShip))
+	{
+		return;
+	}
+	this->SpaceShipActor__ = InSpaceShip;
+	this->SpaceShipActor__.Get()->OnSpaceShipRotate.BindUFunction(this->SpaceRootActor__, TEXT("RotateSpaceRoot"));
+}
+
+void USpaceSalvageWorldSubsystem::RegisterMeteorAvoidance(UMeteorAvoidanceComponent* InAvoidanceComponent)
+{
+}
+
+//void USpaceSalvageWorldSubsystem::RotateSpaceRoot(const FVector2D& InRotationInput)
+//{
+//	if (!IsValid(this->SpaceRootActor__))
+//	{
+//		return;
+//	}
+//	//this->SpaceRootActor__()
+//}
+
+void USpaceSalvageWorldSubsystem::RegisterVirtualMeteor(const FMeteor& InMeteor)
+{
+	this->ActiveVirtualMeteors__.Add(InMeteor);
+	this->MeteorAvoidanceComponent__.Get()->MeteorDetect(InMeteor);
+}
+
+void USpaceSalvageWorldSubsystem::EndOfDay()
+{
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	TimerManager.ClearTimer(this->ItemSpawnHandler__);
+}
+
+void USpaceSalvageWorldSubsystem::SpawnSpaceRoot__()
+{
+	if (this->SpaceRootActor__)
+	{
+		return ;
+	}
+	FActorSpawnParameters SpawnParams;
+
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	ASpaceRootActor*	SpaceRootActor = GetWorld()->SpawnActor<ASpaceRootActor>(
+		ASpaceRootActor::StaticClass(),
+		FVector::ZeroVector,
+		FRotator::ZeroRotator,
+		SpawnParams
+	);
+
+	this->SpaceRootActor__ = SpaceRootActor;
+	UE_LOG(LogTemp, Log, TEXT("SpaceRootActor %s가 할당됐습니다."), *this->SpaceRootActor__.GetName());
+}
+
+void USpaceSalvageWorldSubsystem::SpawnItemActor__()
+{
+	UE_LOG(LogTemp, Log, TEXT("아이템 스폰 시작."));
+	// 테스트용. 나중에 주석 지워야함
+	//if (!this->SpaceShipActor__)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("SpaceShipActor가 nullptr입니다."));
+	//	return ;
+	//}
+	UItemActorFactorySubsystem* ItemFactory = GetWorld()->GetSubsystem<UItemActorFactorySubsystem>();
+
+	if (!ItemFactory)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ItemFactory가 nullptr입니다."));
+		return ;
+	}
+	USceneComponent* ItemPivot = SpaceRootActor__->GetItemPivot();
+
+	if (!IsValid(ItemPivot))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ItemPivot이 nullptr입니다."));
+		return ;
+	}
+	// 테스트용, 나중에 지우고 주석처리한 코드로 교체해야함
+	FVector	SpaceShipForward = this->SpaceRootActor__->GetActorForwardVector();
+	//FVector	SpaceShipForward = this->SpaceShipActor__->GetActorForwardVector();
+	FVector	SpaceShipBackward = -1.0f * SpaceShipForward;
+
+	SpaceShipForward.Y += FMath::FRandRange(-0.2, 0.2);
+	SpaceShipForward.Z += FMath::FRandRange(-0.2, 0.2);
+	FVector		SpawnPos = SpaceShipForward * this->ItemSpawnDist__;
+	FTransform	WorldSpawnTransform(FRotator::ZeroRotator, SpawnPos);
+
+	UItemDataAsset*	TargetItemData = this->SelectSpawnItemData__();
+	if (!TargetItemData)
+	{
+		return ;
+	}
+	AItemActor* ItemActor = ItemFactory->SpawnItemActor(TargetItemData, WorldSpawnTransform);
+
+	// 먼가 바꿔야한대영
+	ItemActor->AttachToComponent(ItemPivot, FAttachmentTransformRules::KeepWorldTransform);
+	SpaceShipBackward.Y += FMath::FRandRange(-0.3, 0.3);
+	SpaceShipBackward.Z += FMath::FRandRange(-0.3, 0.3);
+	float	ItemSpeed = this->ItemMoveSpeed__ * FMath::FRandRange(0.8, 1.2);
+
+	SpaceShipBackward *= ItemSpeed;
+	ItemActor->SetRelativeVelocity(SpaceShipBackward);
+	UE_LOG(LogTemp, Log, TEXT("SpawnItemActor %s가 [%.1f %.1f %.1f]에 생성됐습니다."), *ItemActor->GetName(), SpawnPos.X, SpawnPos.Y, SpawnPos.Z);
+}
+
+void USpaceSalvageWorldSubsystem::UpdateVirtualMeteors__(float CurrentWorldTime)
+{
+}
+
+void USpaceSalvageWorldSubsystem::ResolveMeteor__(FMeteor& Meteor)
+{
+}
+
+UItemDataAsset* USpaceSalvageWorldSubsystem::SelectSpawnItemData__()
+{
+	if (!this->ItemSpawnRateData__)
+	{
+		UE_LOG(LogTemp, Log, TEXT("ItemSpawnRateData가 nullptr입니다."));
+		return (nullptr);
+	}
+	int32	TotalWeight = 0;
+
+	for (auto& RateData : this->ItemSpawnRateData__->ItemSpawnRate)
+	{
+		TotalWeight += RateData.Value;
+	}
+	if (TotalWeight <= 0)
+	{
+		UE_LOG(LogTemp, Log, TEXT("TotalWeight가 0 이하입니다."));
+		return (nullptr);
+	}
+	int32	RandomWeight = FMath::RandRange(0, TotalWeight);
+
+	for (auto& RateData : this->ItemSpawnRateData__->ItemSpawnRate)
+	{
+		if (!IsValid(RateData.Key) || RateData.Value <= 0)
+		{
+			continue ;
+		}
+		RandomWeight -= RateData.Value;
+
+		if (RandomWeight <= 0)
+		{
+			return (RateData.Key.Get());
+		}
+	}
+	UE_LOG(LogTemp, Log, TEXT("아이템이 생성되지 않았습니다."));
+	return (nullptr);
+}
---
+++ ProjectSR/Source/ProjectSR/Public/Framework/Subsystem/SpaceSalvageWorldSubsystem.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Meteor.h"
+#include "SpaceSalvageWorldSubsystem.generated.h"
+
+class ASpaceShipActor;
+class ASpaceRootActor;
+class UMeteorAvoidanceComponent;
+class UItemDataAsset;
+class USpaceMapItemSpawnRateDataAsset;
+//class AMeteorActor;
+
+/**
+ *
+ */
+UCLASS()
+class PROJECTSR_API USpaceSalvageWorldSubsystem : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool	ShouldCreateSubsystem(UObject* Outer) const override;
+
+	virtual void	Initialize(FSubsystemCollectionBase& Collection) override;
+
+	virtual void	OnWorldBeginPlay(UWorld& InWorld) override;
+
+	virtual void	Deinitialize() override;
+
+	virtual void	Tick(float DeltaTime) override;
+
+	virtual TStatId GetStatId() const override
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(
+			USpaceSalvageWorldSubsystem,
+			STATGROUP_Tickables
+		);
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void	SetSpaceMapData(USpaceMapItemSpawnRateDataAsset* InItemRateData);
+	void	RegisterSpaceShipActor(ASpaceShipActor* InSpaceShip);
+	void	RegisterMeteorAvoidance(UMeteorAvoidanceComponent* InAvoidanceComponent);
+	//void	RotateSpaceRoot(const FVector2D& InRotationInput);
+	void	RegisterVirtualMeteor(const FMeteor& InMeteor);
+	void	EndOfDay();
+
+private:
+	void	SpawnSpaceRoot__();
+	void	SpawnItemActor__();
+	void	UpdateVirtualMeteors__(float CurrentWorldTime);
+	void	ResolveMeteor__(FMeteor& Meteor);
+
+	UItemDataAsset*	SelectSpawnItemData__();
+	//void	ReturnMeteor(AMeteorActor* Meteor);
+
+	//AMeteorActor*	AcquireMeteor();
+
+	UPROPERTY()
+	TObjectPtr<ASpaceRootActor>					SpaceRootActor__ = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<ASpaceShipActor>					SpaceShipActor__ = nullptr;
+
+	TWeakObjectPtr<UMeteorAvoidanceComponent>	MeteorAvoidanceComponent__;
+
+	TObjectPtr<USpaceMapItemSpawnRateDataAsset>	ItemSpawnRateData__ = nullptr;
+	// USTRUCT는 값으로 보관
+	TArray<FMeteor> ActiveVirtualMeteors__;
+
+	float	ItemSpawnDist__ = 5000.0f;
+	float	ItemMoveSpeed__ = 300.0f;
+
+	// Actor는 UObject이므로 TObjectPtr 사용 가능
+	//UPROPERTY()
+	//TArray<TObjectPtr<AMeteorActor>> InactiveMeteorPool_;
+
+	FTimerHandle	ItemSpawnHandler__;
+	float			ItemSpawnTimer__ = 0.0f;
+
+};
