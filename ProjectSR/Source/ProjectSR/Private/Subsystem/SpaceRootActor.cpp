ede19ba0e7f69365c4c68b538c4ab597e84c0df2
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-08-28 15:29:41 +0900

    feat: 우주선 회전 대신 처리할 액터 추가
    우주선 중심으로 입력받은 회전의 역방향으로 회전할 액터
    
    우주에 스폰된 우주 쓰레기 및 운석 생성 좌표의 중심점

---
+++ ProjectSR/Source/ProjectSR/Private/Subsystem/SpaceRootActor.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystem/SpaceRootActor.h"
+
+// Sets default values
+ASpaceRootActor::ASpaceRootActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	this->RootScene_ = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	SetRootComponent(this->RootComponent);
+	SetActorLocation(FVector::ZeroVector);
+	SetActorRotation(FRotator::ZeroRotator);
+
+	this->BackgroundPivot_ = CreateDefaultSubobject<USceneComponent>(TEXT("BackgroundPivot"));
+	this->BackgroundPivot_->SetupAttachment(this->RootScene_);
+
+	this->ItemPivot_ = CreateDefaultSubobject<USceneComponent>(TEXT("ItemPivot"));
+	this->ItemPivot_->SetupAttachment(this->RootScene_);
+
+	this->SpawnShellPivot_ = CreateDefaultSubobject<USceneComponent>(TEXT("SpawnShellPivot"));
+	this->SpawnShellPivot_->SetupAttachment(this->RootScene_);
+
+	this->MeteorPivot_ = CreateDefaultSubobject<USceneComponent>(TEXT("MeteorPivot"));
+	this->MeteorPivot_->SetupAttachment(this->RootScene_);
+}
+
+// Called when the game starts or when spawned
+void ASpaceRootActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ASpaceRootActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+//void ASpaceRootActor::RotateSpaceRoot(const FVector2D& InInput, const float InRotateSpeed)
+//{
+//	float	DeltaTime = GetWorld()->GetDeltaSeconds();
+//
+//	FVector2D	InputToRotate = InInput.GetClampedToMaxSize(1.0f) * InRotateSpeed * DeltaTime;
+//
+//	FQuat DeltaRotation = FRotator(
+//		-InputToRotate.Y,	// Pitch
+//		-InputToRotate.X,	// Yaw
+//		0.0f				// Roll
+//	).Quaternion();
+//
+//	AddActorLocalRotation(DeltaRotation);
+//}
+
+void ASpaceRootActor::RotateSpaceRoot(const FRotator& InRotate)
+{
+	this->SetActorRotation(InRotate);
+}
