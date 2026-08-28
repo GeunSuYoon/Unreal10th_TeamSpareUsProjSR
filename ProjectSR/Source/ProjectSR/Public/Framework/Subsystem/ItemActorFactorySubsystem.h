013e171e85890e51d224ef089049ac7d6a56d52c
Seongho Jin <snskrktja@naver.com>
2026-08-28 16:26:27 +0900

    chore: private 변수, 함수 이름 수정

--- ProjectSR/Source/ProjectSR/Public/Framework/Subsystem/ItemActorFactorySubsystem.h
+++ ProjectSR/Source/ProjectSR/Public/Framework/Subsystem/ItemActorFactorySubsystem.h
@@ -43,12 +43,12 @@ public:
 
 private:
     // 실제 스폰을 처리할 함수
-    AItemActor* SpawnProcess(const UItemDataAsset* InItemData, const FTransform& InTransform);
+    AItemActor* SpawnProcess__(const UItemDataAsset* InItemData, const FTransform& InTransform);
 
     // 비동기 작업의 핸들 중 완료된 핸들을 정리하는 함수
-    void CleanupCompletedHandles();
+    void CleanupCompletedHandles__();
 
 private:
     // 진행중인 비동기 작업의 핸들 모음
-    TArray<TSharedPtr<FStreamableHandle>> ActiveStreamableHandles;
+    TArray<TSharedPtr<FStreamableHandle>> ActiveStreamableHandles__;
 };
