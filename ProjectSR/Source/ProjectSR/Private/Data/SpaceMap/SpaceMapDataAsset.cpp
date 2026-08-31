f2d8430da4c8fb59ae5175f9ac37ec991db9d8e4
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-08-31 10:15:47 +0900

    refact: SpaceMapItemSpawnRateDataAsset의 이름을 SpaceMapDataAsset으로 변경
    SpaceMapItemSpawnRateDataAsset에 맵의 다른 정보들을 담는다면 기존 이름보다 SpaceMapDataAsset이 맞다고 판단해 이름 변경

--- ProjectSR/Source/ProjectSR/Private/Data/SpaceMap/SpaceMapDataAsset.cpp
+++ ProjectSR/Source/ProjectSR/Private/Data/SpaceMap/SpaceMapDataAsset.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Data/SpaceMap/SpaceMapItemSpawnRateDataAsset.h"
+#include "Data/SpaceMap/SpaceMapDataAsset.h"
 
