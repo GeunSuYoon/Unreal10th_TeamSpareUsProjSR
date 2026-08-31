c641d33968f6709d05aac65c0b3c5e5931bc876b
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-08-31 17:41:01 +0900

    feat: 새로운 운석 구조체 생성, 운석 충돌 판정 함수 구현중
    그래요

--- ProjectSR/Source/ProjectSR/Private/SpaceShip/MeteorAvoidanceComponent.cpp
+++ ProjectSR/Source/ProjectSR/Private/SpaceShip/MeteorAvoidanceComponent.cpp
@@ -2,13 +2,14 @@
 
 
 #include "SpaceShip/MeteorAvoidanceComponent.h"
+#include "Data/SpaceMap/SpaceMapDataAsset.h"
 
 // Sets default values for this component's properties
 UMeteorAvoidanceComponent::UMeteorAvoidanceComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -32,21 +33,84 @@ void UMeteorAvoidanceComponent::TickComponent(float DeltaTime, ELevelTick TickTy
 	// ...
 }
 
-void UMeteorAvoidanceComponent::MeteorDetect(const FMeteor& InMeteor)
+void UMeteorAvoidanceComponent::MeteorDetect(const USpaceMapDataAsset* InSpaceMapData)
 {
-	this->TargetMeteor_ = InMeteor;
-	//this->TargetMeteor_ = InMeteor;
-}
-
-void UMeteorAvoidanceComponent::SpaceShipRotateChange(const FRotator& InRotate)
-{
-	this->SpaceShipForward_ = InRotate.Vector();
-	if (!this->TargetMeteor_)
+	if (!InSpaceMapData)
 	{
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("[UMeteorAvoidanceComponent::MeteorDetect] InSpaceMapData가 nullptr입니다.")
+		);
 		return ;
 	}
+	if (InSpaceMapData->MeteorAlarmTimer <= 0.0f
+		|| InSpaceMapData->MeteorSpawnLeadTime < 0.0f
+		|| InSpaceMapData->MeteorSpawnLeadTime >= InSpaceMapData->MeteorAlarmTimer
+		|| InSpaceMapData->MeteorSpeed <= 0.0f)
+	{
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("[UMeteorAvoidanceComponent::MeteorDetect] InSpaceMapData [%s]의 값이 이상합니다."),
+			*InSpaceMapData->MapName.ToString()
+		);
+		return ;
+	}
+	this->TargetMeteor_.MeteorRemainTime = InSpaceMapData->MeteorAlarmTimer;
+	this->TargetMeteor_.MeteorSpawnLeadTime = InSpaceMapData->MeteorSpawnLeadTime;
+	this->TargetMeteor_.MeteorDamage = InSpaceMapData->MeteorDamage;
+	this->TargetMeteor_.MeteorSpeed = InSpaceMapData->MeteorSpeed;
+	this->TargetMeteor_.MeteorSize = InSpaceMapData->MeteorSize;
+	this->TargetMeteor_.MoveDir = FVector(
+		FMath::FRandRange(-1.0f, 1.0f),
+		FMath::FRandRange(-1.0f, 1.0f),
+		FMath::FRandRange(-1.0f, 1.0f)
+	);
+	this->TargetMeteor_.MoveDir.Normalize();
+	this->TargetMeteor_.SpawnPos = -1.0f * this->TargetMeteor_.MoveDir * InSpaceMapData->ItemSpawnDist;
+	this->TargetMeteor_.DespawnPos = this->TargetMeteor_.MoveDir * InSpaceMapData->ItemSpawnDist;
+	this->bIsMeteor__ = true;
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	TimerManager.SetTimer(
+		this->MeteorAlarmTimer__,
+		this,
+		&UMeteorAvoidanceComponent::MeteorAlarm,
+		this->MeteorAlarmTime__,
+		true,
+		0.0f
+	);
+}
+
+void UMeteorAvoidanceComponent::SpaceShipMoveInput(const FVector2D& InMoveInput, const float InSpaceShipSpeed)
+{
+	FVector2D	MeteorMoveDir = FVector2D(-InMoveInput.Y, -InMoveInput.X);
+
+
+	OnMeteorAvoid.ExecuteIfBound(this->TargetMeteor_);
 }
 
 void UMeteorAvoidanceComponent::EvaluateMeteorAvoidance()
 {
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	TimerManager.ClearTimer(this->MeteorAlarmTimer__);
+}
+
+void UMeteorAvoidanceComponent::MeteorAlarm()
+{
+	this->TargetMeteor_.MeteorRemainTime -= this->MeteorAlarmTime__;
+	if (this->TargetMeteor_.MeteorRemainTime <= this->TargetMeteor_.MeteorSpawnLeadTime)
+	{
+		this->EvaluateMeteorAvoidance();
+		return ;
+	}
+	OnMeteorDetect.ExecuteIfBound();
+}
+
+void UMeteorAvoidanceComponent::ClearMeteor()
+{
+	this->TargetMeteor_.Clear();
+	this->bIsMeteor__ = false;
 }
