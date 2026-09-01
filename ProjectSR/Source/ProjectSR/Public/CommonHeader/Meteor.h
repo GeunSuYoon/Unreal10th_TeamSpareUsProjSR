e074998bdcafb2577e9e28bbbb19943527011e4e
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-01 11:45:02 +0900

    refact: 운석 구조체 CommonHeader로 옮김
    기존에 MeteorAvoidanceComponent에 존재하던 운석 구조체를 따로 빼서 CommonHeader/Meteor.h로 옮김

---
+++ ProjectSR/Source/ProjectSR/Public/CommonHeader/Meteor.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Meteor.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMeteor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	float	MeteorRemainTime = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	float	MeteorSpawnLeadTime = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	float	MeteorDamage = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	float	MeteorSpeed = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	float	MeteorSize = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	FVector	MoveDir = FVector::Zero();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	FVector	StartPos = FVector::Zero();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	FVector	EndPos = FVector::Zero();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Meteor")
+	FVector	ClosestApproachPos = FVector::Zero();
+
+	void	Clear()
+	{
+		MeteorRemainTime = 0.0f;
+		MeteorSpawnLeadTime = 0.0f;
+		MeteorDamage = 0.0f;
+		MeteorSpeed = 0.0f;
+		MeteorSize = 0.0f;
+		MoveDir = FVector::Zero();
+		StartPos = FVector::Zero();
+		EndPos = FVector::Zero();
+		ClosestApproachPos = FVector::Zero();
+	}
+};
+
+/**
+ *
+ */
