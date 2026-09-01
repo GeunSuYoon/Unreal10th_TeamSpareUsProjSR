e388d5f97c40bbe5e0e5c560e2f7c301317b9a77
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-01 11:43:51 +0900

    chore: Lazer 오타 수정
    Lazor > Lazer

--- ProjectSR/Source/ProjectSR/Public/SpaceShip/LazerComponent.h
+++ ProjectSR/Source/ProjectSR/Public/SpaceShip/LazerComponent.h
@@ -6,7 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "LazerComponent.generated.h"
 
-class ULazorDataAsset;
+class ULazerDataAsset;
 class UMeteo;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -22,28 +22,32 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 public:
 	// 레이저 데이터를 세팅하는 함수 다른 값도 해당 데이터의 값으로 변경된다.
-	void	SetLazorData(ULazorDataAsset* InLazorData);
+	void	SetLazerData(ULazerDataAsset* InLazerData);
+
 	// private 멤버 변수 getter 함수.
-	inline float	GetLazorPower() const { return (this->LazorPower__); }
-	inline float	GetUseEnergy() const { return (this->UseEnergy__); }
+	inline int32	GetLevel() const { return (this->Level_); }
+	inline float	GetLazerPower() const { return (this->LazerDamage__); }
 	inline float	GetReactiveEnergy() const { return (this->ReactiveEnergy__); }
+	inline float	GetOperationalEnergy() const { return (this->OperationalEnergy__); }
 
 	// 운석 날라올 때 격추하려 불러오는 함수. TODO: 다른 팀원의 구현에 따라 선언 타입 및 내부 로직 변경 필요.
 	void	AttackMeteo(UMeteo* InMeteo);
 
 protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32	Level_ = 0;
 
 private:
+	TObjectPtr<ULazerDataAsset>	LazerData__ = nullptr;
 
-	TObjectPtr<ULazorDataAsset>	LazorData__ = nullptr;
-
-	float	LazorPower__ = 0.0f;
-	float	UseEnergy__ = 0.0f;
+	float	LazerDamage__ = 0.0f;
 	float	ReactiveEnergy__ = 0.0f;
+	float	OperationalEnergy__ = 0.0f;
 };
