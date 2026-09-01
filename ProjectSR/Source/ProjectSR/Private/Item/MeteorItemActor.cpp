27a9db5fad801b9b4f3da24e99a9adeade27ee90
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-01 17:06:52 +0900

    feat: 운석 초기화 함수 및 제거 로직 추가
    운석의 값을 초기화하는 함수 추가
    
    운석 제거 로직은 나중에 spaceshipactor를 만들고 충돌시 없애게 만들 예정

--- ProjectSR/Source/ProjectSR/Private/Item/MeteorItemActor.cpp
+++ ProjectSR/Source/ProjectSR/Private/Item/MeteorItemActor.cpp
@@ -3,3 +3,22 @@
 
 #include "Item/MeteorItemActor.h"
 
+void AMeteorItemActor::InitMeteor(const FMeteor& InMeteor, const FVector& ShipCenter, float InDespawnDist)
+{
+	this->ClosestApproachWorldPos__ = ShipCenter + InMeteor.ClosestApproachPos;
+	this->MoveDir__ = InMeteor.MoveDir;
+	this->DespawnDist__ = InDespawnDist;
+}
+
+void AMeteorItemActor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	FVector	FromClosestPoint = GetActorLocation() - this->ClosestApproachWorldPos__;
+	float	PassedDistance = FVector::DotProduct(FromClosestPoint, this->MoveDir__);
+
+	if (PassedDistance >= this->DespawnDist__)
+	{
+		FinishUsingPoolable();
+	}
+}
--- ProjectSR/Source/ProjectSR/Public/Item/MeteorItemActor.h
+++ ProjectSR/Source/ProjectSR/Public/Item/MeteorItemActor.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CommonHeader/Meteor.h"
+
 #include "Item/ItemActor.h"
 #include "MeteorItemActor.generated.h"
 
@@ -15,9 +17,21 @@ class PROJECTSR_API AMeteorItemActor : public AItemActor
 	GENERATED_BODY()
 	
 public:
+	void	InitMeteor(
+		const FMeteor& InMeteor,
+		const FVector& ShipCenter,
+		float InDespawnDistance
+	);
+
 	void	SetDamage(float InDamage) { this->Damage__ = InDamage; }
 	void	LazerDamage(float InDamage) { this->Damage__ -= InDamage; }
 
+	virtual void Tick(float DeltaSeconds) override;
+
 private:
 	float	Damage__ = 0.0f;
+	float	DespawnDist__ = 0.0f;
+	FVector	ClosestApproachWorldPos__ = FVector::Zero();
+	FVector	MoveDir__ = FVector::Zero();
+
 };
