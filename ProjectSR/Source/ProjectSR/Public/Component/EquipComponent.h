db8f68df357fa93808e5e7e717b34e64cf7083f1
화영 이 <duddl7209@naver.com>
2026-09-01 11:21:02 +0900

    feat: Interface 구현, EquipComponent 구현

--- ProjectSR/Source/ProjectSR/Public/Component/EquipComponent.h
+++ ProjectSR/Source/ProjectSR/Public/Component/EquipComponent.h
@@ -6,6 +6,18 @@
 #include "Components/ActorComponent.h"
 #include "EquipComponent.generated.h"
 
+// 장비 슬롯 구조체
+UENUM(BlueprintType)
+enum class EEquipmentSlot : uint8
+{
+	None		UMETA(DisplayName = "None"),
+	SpaceSuit	UMETA(DisplayName = "SpaceSuit"),
+	DragMachine	UMETA(DisplayName = "DragMachine"),
+	Weapon		UMETA(DisplayName = "Weapon")
+};
+
+// 장비 교체 이벤트 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnEquipmentChangedSignature, EEquipmentSlot, Slot, AActor*, EquippedSlot);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PROJECTSR_API UEquipComponent : public UActorComponent
@@ -15,6 +27,27 @@ class PROJECTSR_API UEquipComponent : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UEquipComponent();
+
+	// 슬롯별 장비 장착-해제 함수
+	UFUNCTION(BlueprintCallable, Category = "Equip")
+	bool EquipItem(EEquipmentSlot Slot, TSubclassOf<AActor> EquipActorClass);
+
+	UFUNCTION(BlueprintCallable, Category = "Equip")
+	bool UnEquipItem(EEquipmentSlot Slot);
+	
+	// 현재 장착된 액터 가져오기
+	UFUNCTION(BlueprintCallable, Category = "Equip")
+	AActor* GetEquippedActor(EEquipmentSlot Slot) const;
+
+public:
+	// 스탯, 인벤토리 컴포넌트나 UI가 구독할 델리게이트
+	UPROPERTY(BlueprintAssignable, Category = "Equip|Events")
+	FOnEquipmentChangedSignature OnEquipmentChanged;
+
+protected:
+	// 슬롯별 현재 생성되어 장착된 액터들을 관리하는 맵
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Equip")
+	TMap<EEquipmentSlot, AActor*> EquippedActors;
 
 protected:
 	// Called when the game starts
