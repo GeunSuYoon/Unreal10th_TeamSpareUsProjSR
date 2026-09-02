b327703801f0a3b917003b123c6a8798e1254146
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-02 16:09:32 +0900

    feat: SpaceShipActor에 DoorButtonActor 추가
    DoorButtonActor 추가
    DoorButtonActor의 델리게이터에 반응하는 DetectDoorButtonClick_ 함수 추가
    실제로 여닫힐 문인 DoorMesh 추가
    
    기타 보유 액터들의 생성 및 RootComponent에 Attach하는 로직 추가
    보유 액터들의 실제 액터 타입으로 저장하는 로직 추가

--- ProjectSR/Source/ProjectSR/Private/SpaceShip/SpaceShipActor.cpp
+++ ProjectSR/Source/ProjectSR/Private/SpaceShip/SpaceShipActor.cpp
@@ -3,6 +3,12 @@
 
 #include "SpaceShip/SpaceShipActor.h"
 #include "SpaceShip/SpaceShipVisualActor.h"
+#include "SpaceShip/LazerComponent.h"
+#include "SpaceShip/MachineArmComponent.h"
+#include "SpaceShip/MeteorAvoidanceComponent.h"
+#include "SpaceShip/DoorButtonActor.h"
+#include "Component/InventoryComponent.h"
+#include "Component/CraftingComponent.h"
 #include "MainPanel/MainPanelActor.h"
 
 // Sets default values
@@ -16,7 +22,16 @@ ASpaceShipActor::ASpaceShipActor()
 	SetRootComponent(RootSceneComponent);
 	
 	this->SpaceShipVisualActor_ = CreateDefaultSubobject<UChildActorComponent>(TEXT("SpaceShipVisual"));
-	this->SpaceShipVisualActor_->SetupAttachment(RootSceneComponent);
+	this->SpaceShipVisualActor_->SetupAttachment(GetRootComponent());
+
+	this->MainPanel_ = CreateDefaultSubobject<UChildActorComponent>(TEXT("MainPanel"));
+	this->MainPanel_->SetupAttachment(GetRootComponent());
+
+	this->DoorButton_ = CreateDefaultSubobject<UChildActorComponent>(TEXT("DoorButton"));
+	this->DoorButton_->SetupAttachment(GetRootComponent());
+
+	this->DoorMesh_ = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+	this->DoorMesh_->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
@@ -25,17 +40,70 @@ void ASpaceShipActor::BeginPlay()
 	Super::BeginPlay();
 	
 	this->SpaceShipRotateState_ = FRotator::ZeroRotator;
-	FActorSpawnParameters Params;
-	Params.Owner = this;
-	this->MainPanel_ = GetWorld()->SpawnActor<AMainPanelActor>(
-		AMainPanelActor::StaticClass(),
-		GetActorTransform(),
-		Params
-	);
 	if (this->MainPanel_)
 	{
-		this->MainPanel_->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
-		this->MainPanel_->OnSpaceShipRotateInput.BindUFunction(this, TEXT("SpaceShipRotateInput_"));
+		this->MainPanelActor_ = Cast<AMainPanelActor>(this->MainPanel_->GetChildActor());
+		if (this->MainPanelActor_ == nullptr)
+		{
+			UE_LOG(
+				LogTemp,
+				Error,
+				TEXT("[ASpaceShipActor::BeginPlay] MainPanel의 실제 타입이 AMainPanelActor가 아닙니다.")
+			);
+		}
+	}
+	else
+	{
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("[ASpaceShipActor::BeginPlay] MainPanel이 nullptr입니다.")
+		);
+	}
+	if (this->DoorButton_)
+	{
+		this->DoorButtonActor_ = Cast<ADoorButtonActor>(this->DoorButton_->GetChildActor());
+		if (this->DoorButtonActor_ == nullptr)
+		{
+			UE_LOG(
+				LogTemp,
+				Error,
+				TEXT("[ASpaceShipActor::BeginPlay] DoorButton의 실제 타입이 ADoorButtonActor가 아닙니다.")
+			);
+		}
+		this->DoorButtonActor_->OnDoorButtonClick.BindUFunction(this, TEXT("DetectDoorButtonClick_"));
+	}
+	else
+	{
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("[ASpaceShipActor::BeginPlay] DoorButton이 nullptr입니다.")
+		);
+	}
+	if (this->DoorMesh_)
+	{
+		DoorTargetRotate_ = this->DoorMesh_->GetRelativeRotation();
+	}
+	else
+	{
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("[ASpaceShipActor::BeginPlay] DoorMesh가 nullptr입니다.")
+		);
+	}
+	if (this->MeteorAvoidanceComponent_)
+	{
+		this->MeteorAvoidanceComponent_->SetSpaceShipSafeArea(this->SafeArea_);
+	}
+	else
+	{
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("[ASpaceShipActor::BeginPlay] MeteorAvoidanceComponent가 nullptr입니다.")
+		);
 	}
 }
 
@@ -48,7 +116,7 @@ void ASpaceShipActor::Tick(float DeltaTime)
 
 UInventoryComponent* ASpaceShipActor::GetInventoryComponent_Implementation()
 {
-	return (this->Warehouse_);
+	return (this->WarehouseComponent_);
 }
 
 float ASpaceShipActor::RequestEnergy(float InEnergy)
@@ -61,7 +129,7 @@ float ASpaceShipActor::RequestEnergy(float InEnergy)
 
 void ASpaceShipActor::RepairDurability_Implementation(float InDurability)
 {
-	this->CurrentDurability_ = FMath::Min(this->CurrentDurability_ + InDurability, this->MaxDurability_);
+	this->CurrentDurability_ = FMath::Max(this->CurrentDurability_ + InDurability, this->MaxDurability_);
 }
 
 void ASpaceShipActor::ConsumDurability_Implementation(float InDurability)
@@ -69,17 +137,76 @@ void ASpaceShipActor::ConsumDurability_Implementation(float InDurability)
 	this->CurrentDurability_ = FMath::Min(this->CurrentDurability_ - InDurability, 0.0f);
 }
 
-void ASpaceShipActor::SpaceShipRotateInput_(const FVector2D& InInput)
+void ASpaceShipActor::DetectDoorButtonClick_()
 {
-	float	DeltaTime = GetWorld()->GetDeltaSeconds();
-
-	FVector2D	InputToRotate = InInput.GetClampedToMaxSize(1.0f) * this->RotateSpeed_ * DeltaTime;
-
-	this->SpaceShipRotateState_ += FRotator(
-		-InputToRotate.Y,	// Pitch
-		-InputToRotate.X,	// Yaw
-		0.0f				// Roll
+	if (!this->DoorMesh_)
+	{
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("[ADoorButtonActor::Interact_Implementation] DoorMesh가 설정되지 않았습니다.")
+		);
+		return ;
+	}
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("[ADoorButtonActor::Interact_Implementation] 문이 열리거나 닫힙니다.")
 	);
-	this->OnSpaceShipRotate.ExecuteIfBound(this->SpaceShipRotateState_);
-	//AddActorLocalRotation(DeltaRotation);
+
+	FRotator	DoorRotation = this->DoorMesh_->GetRelativeRotation();
+
+	this->DoorTargetRotate_.Roll = this->bIsDoorOpen_
+			? this->DoorCloseAngle_
+			: this->DoorOpenAngle_;
+	this->bIsDoorOpen_ = !this->bIsDoorOpen_;
+	GetWorldTimerManager().SetTimer(
+		this->DoorMoveTimerHandle_,
+		this,
+		&ASpaceShipActor::UpdateDoorRotation_,
+		this->DoorInteractTime_,
+		true
+	);
 }
+
+void	ASpaceShipActor::UpdateDoorRotation_()
+{ 
+	FRotator	DoorRotation = this->DoorMesh_->GetRelativeRotation();
+	FRotator	NowRotate = FMath::RInterpConstantTo(DoorRotation, this->DoorTargetRotate_, this->DoorInteractTime_, this->DoorRotationSpeed_);
+
+	//UE_LOG(
+	//	LogTemp,
+	//	Log,
+	//	TEXT("[ADoorButtonActor::Interact_Implementation] 문이 움직입니다.")
+	//);
+	this->DoorMesh_->SetRelativeRotation(NowRotate);
+	if (FMath::IsNearlyEqual(NowRotate.Roll, this->DoorTargetRotate_.Roll))
+	{
+		//UE_LOG(
+		//	LogTemp,
+		//	Log,
+		//	TEXT("[ADoorButtonActor::Interact_Implementation] 문이 움직입니다.")
+		//);
+		GetWorldTimerManager().ClearTimer(this->DoorMoveTimerHandle_);
+	}
+}
+
+void	ASpaceShipActor::MeteorDetect(const USpaceMapDataAsset* InMapData)
+{
+	this->MeteorAvoidanceComponent_->MeteorDetect(InMapData);
+}
+
+//void ASpaceShipActor::SpaceShipRotateInput_(const FVector2D& InInput)
+//{
+//	float	DeltaTime = GetWorld()->GetDeltaSeconds();
+//
+//	FVector2D	InputToRotate = InInput.GetClampedToMaxSize(1.0f) * this->RotateSpeed_ * DeltaTime;
+//
+//	this->SpaceShipRotateState_ += FRotator(
+//		-InputToRotate.Y,	// Pitch
+//		-InputToRotate.X,	// Yaw
+//		0.0f				// Roll
+//	);
+//	this->OnSpaceShipRotate.ExecuteIfBound(this->SpaceShipRotateState_);
+//	//AddActorLocalRotation(DeltaRotation);
+//}
--- ProjectSR/Source/ProjectSR/Public/SpaceShip/SpaceShipActor.h
+++ ProjectSR/Source/ProjectSR/Public/SpaceShip/SpaceShipActor.h
@@ -8,11 +8,17 @@
 #include "Interface/InventoryComponentInterface.h"
 #include "SpaceShipActor.generated.h"
 
-class AMainPanelActor;
+class USpaceMapDataAsset;
+
 class ULazerComponent;
 class UMachineArmComponent;
 class UInventoryComponent;
+class UCraftingComponent;
+class UMeteorAvoidanceComponent;
+
+class AMainPanelActor;
 class ASpaceShipVisualActor;
+class ADoorButtonActor;
 
 DECLARE_DYNAMIC_DELEGATE_OneParam(FOnSpaceShipRotate, const FRotator&, InSpaceShipRotate);
 
@@ -42,13 +48,15 @@ public:
 	inline float	GetMoveSpeed() const { return (this->MoveSpeed_); }
 	inline float	GetSafeArea() const { return (this->SafeArea_); }
 
-	UMeteorAvoidanceComponent*	GetMeteorAvoidance() const { return (this->MeteorAvoidance_); }
+	UMeteorAvoidanceComponent*	GetMeteorAvoidance() const { return (this->MeteorAvoidanceComponent_); }
 	//inline UInventoryComponent*	GetWarehouse() const { return (this->Warehouse_); }
 
 	virtual UInventoryComponent*	GetInventoryComponent_Implementation() override;
 
 	// 하루 끝날 때 불러올 함수
 	void	EndOfDay();
+
+	void	MeteorDetect(const USpaceMapDataAsset* InMapData);
 
 	// CurrentEnergy에서 에너지를 요청하는 함수
 	float	RequestEnergy(float InEnergy);
@@ -58,28 +66,70 @@ public:
 
 	FOnSpaceShipRotate	OnSpaceShipRotate;
 
+	UFUNCTION(BlueprintCallable)
+	ADoorButtonActor*	GetDoorButtonActor() { return (this->DoorButtonActor_); }
+
 protected:
 	void	SpaceShipRotateInput_(const FVector2D& InInput);
 
+	UFUNCTION()
+	void	DetectDoorButtonClick_();
+	void	UpdateDoorRotation_();
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Visual")
-	TObjectPtr<UChildActorComponent>	SpaceShipVisualActor_ = nullptr;
+	TObjectPtr<UChildActorComponent>		SpaceShipVisualActor_ = nullptr;
 
 	// 지금 우주선의 강화 단계
 	UPROPERTY(BlueprintReadOnly, Category = "Level")
 	int32	Level_ = 0;
 
+	UPROPERTY(BlueprintReadOnly, Category = "MainPanel")
+	TObjectPtr<UChildActorComponent>		MainPanel_ = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MainPanel")
+	TObjectPtr<AMainPanelActor>	MainPanelActor_ = nullptr;
+
+	// 문 여닫기 관련 변수들
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DoorButton")
+	TObjectPtr<UChildActorComponent>		DoorButton_ = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DoorButton")
+	TObjectPtr<ADoorButtonActor>			DoorButtonActor_ = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DoorButton")
+	TObjectPtr<UStaticMeshComponent>		DoorMesh_ = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DoorButton")
+	float			DoorInteractTime_ = 0.02f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DoorButton")
+	float			DoorOpenAngle_ = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DoorButton")
+	float			DoorCloseAngle_ = 90.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DoorButton")
+	float			DoorRotationSpeed_ = 90.0f;
+
+	FTimerHandle	DoorMoveTimerHandle_;
+
+	FRotator		DoorTargetRotate_;
+
+	bool			bIsDoorOpen_ = false;
+
+
 	// 우주선이 가지고 있는 컴포넌트
 	UPROPERTY(BlueprintReadOnly, Category = "Component")
-	TObjectPtr<AMainPanelActor>			MainPanel_;
+	TObjectPtr<ULazerComponent>				LazerComponent_ = nullptr;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Component")
-	TObjectPtr<ULazerComponent>			Lazor_;
+	TObjectPtr<UMachineArmComponent>		MainArmComponent_ = nullptr;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Component")
-	TObjectPtr<UMachineArmComponent>	MainArm_;
+	TObjectPtr<UInventoryComponent>			WarehouseComponent_ = nullptr;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Component")
-	TObjectPtr<UInventoryComponent>		Warehouse_;
+	TObjectPtr<UMeteorAvoidanceComponent>	MeteorAvoidanceComponent_ = nullptr;
 
 	// 우주선 내구도 관련 변수
 	UPROPERTY(BlueprintReadOnly, Category = "Durability")
@@ -105,7 +155,9 @@ protected:
 	UPROPERTY(BlueprintReadOnly, Category = "Rotate")
 	FRotator	SpaceShipRotateState_;
 
-	UPROPERTY(BlueprintReadOnly, Category = "Area")
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "SafeArea")
 	float	SafeArea_ = 100.0f;
 
 };
