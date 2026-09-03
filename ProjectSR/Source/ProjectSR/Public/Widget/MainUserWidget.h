5277b62fecec054bc7b9f0eea7017da6cb5b9b29
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-03 17:04:52 +0900

    feat: HUD에서 viewport에 추가할 MainUserWidget 추가
    모든 상시 띄워질 하위 위젯들은 이 위젯에서 관리할 예정

---
+++ ProjectSR/Source/ProjectSR/Public/Widget/MainUserWidget.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainUserWidget.generated.h"
+
+class UMeteorWarningUserWidget;
+class ASpaceShipActor;
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSR_API UMainUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void	BindToPlayer();
+	void	BindToSpaceShip(ASpaceShipActor* InSpaceShipActor);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<UMeteorWarningUserWidget>	MeteoWarningWidget;
+
+};
