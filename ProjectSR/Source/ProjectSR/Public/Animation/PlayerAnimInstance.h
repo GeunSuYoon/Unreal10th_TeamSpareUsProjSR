d967b5167b94c828b8f85d78fb371fe2d1c81b4b
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-03 17:05:37 +0900

    test: 해당 주석이 없으면 컴파일 오류가 나용

--- ProjectSR/Source/ProjectSR/Public/Animation/PlayerAnimInstance.h
+++ ProjectSR/Source/ProjectSR/Public/Animation/PlayerAnimInstance.h
@@ -16,10 +16,10 @@ class PROJECTSR_API UPlayerAnimInstance : public UAnimInstance
 	GENERATED_BODY()
 	
 public:
-	// BeginPlay 역할
-	virtual void NativeInitializeAnimation() override;
-	// Tick 역할
-	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	//// BeginPlay 역할
+	//virtual void NativeInitializeAnimation() override;
+	//// Tick 역할
+	//virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 
 protected:
 	// 소유 캐릭터 참조 (캐스팅 비용 절감)
