9c293f45768e17a30d2dc4592ff95b86f4a5c3fe
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-04 16:20:21 +0900

    feat: MeteorAvoidanceComponent의 실제 상황을 보여주는 위젯 추가
    MeteorEventUserWidget
    가운데 우주선을 띄우는 위젯
    운석이 감지되면 MeteorRouteUserWidget의 크기와 각도를 조절해 지금 운석의 이동경로를 보여준다.
    
    MeteorRouteUserWidget
    운석의 이동경로를 나타낼 위젯

---
+++ ProjectSR/Source/ProjectSR/Private/Widget/MeteorEventUserWidget.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/MeteorEventUserWidget.h"
+#include "Widget/MeteorRouteUserWidget.h"
+#include "SpaceShip/SpaceShipActor.h"
+#include "SpaceShip/MeteorAvoidanceComponent.h"
+#include "Framework/Subsystem/SpaceSalvageWorldSubsystem.h"
+
+#include "Components/SizeBox.h"
+#include "Components/CanvasPanelSlot.h"
+
+void UMeteorEventUserWidget::BindToSpaceShip(ASpaceShipActor* InSpaceShip)
+{
+	UMeteorAvoidanceComponent* Component = InSpaceShip->GetMeteorAvoidanceComponent();
+
+	this->SpaceShipSize__ = InSpaceShip->GetSafeAreaRadius();
+	Component->OnMeteorDetect.AddDynamic(this, &UMeteorEventUserWidget::OnMeteorDetect__);
+	Component->OnMeteorMove.AddDynamic(this, &UMeteorEventUserWidget::SetMeteorRoute);
+	Component->OnMeteorClear.AddDynamic(this, &UMeteorEventUserWidget::OnMeteorClear__);
+}
+
+void UMeteorEventUserWidget::SetWidgetSize(const float InSapwnDist)
+{
+	this->PixelPerUnit = this->MapSidePx__ / (2.0f * InSapwnDist);
+	this->SpaceMapSpawnDist__ = InSapwnDist;
+	float	SpaceShipWidgetSize = PixelPerUnit * 2.0f * this->SpaceShipSize__;
+
+	this->SpaceShipImageSize->SetWidthOverride(SpaceShipWidgetSize);
+	this->SpaceShipImageSize->SetHeightOverride(SpaceShipWidgetSize);
+}
+
+void UMeteorEventUserWidget::SetMeteorRoute(const FMeteor& InMeteor)
+{
+	if (!this->MeteorRouteCanvasSlot)
+	{
+		return;
+	}
+	FVector	RouteCenter3D = (InMeteor.StartPos + InMeteor.EndPos) * 0.5f;
+	FVector	RouteDirection3D = InMeteor.MoveDir;
+
+	FVector2D	RouteCenter2D = this->MapCenter2D__ + FVector2D(-RouteCenter3D.Y, RouteCenter3D.Z) * this->PixelPerUnit;
+	FVector2D	RouteDirection2D = FVector2D(-RouteDirection3D.Y, RouteDirection3D.Z);
+	float		RouteAngle = FMath::RadiansToDegrees(
+		FMath::Atan2(
+			RouteDirection2D.Y,
+			RouteDirection2D.X
+		)) + 90.0f;
+
+	this->MeteorRouteSize->SetWidthOverride(this->PixelPerUnit * 2.0f * InMeteor.MeteorSize);
+	this->MeteorRouteSize->SetRenderTransformPivot(FVector2D(0.5f, 0.5f));
+	this->MeteorRouteSize->SetRenderTransformAngle(RouteAngle);
+	this->MeteorRouteCanvasSlot->SetPosition(RouteCenter2D);
+}
+
+void UMeteorEventUserWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	USpaceSalvageWorldSubsystem* SpaceSubsystem = GetWorld()->GetSubsystem<USpaceSalvageWorldSubsystem>();
+	if (IsValid(SpaceSubsystem))
+	{
+		SpaceSubsystem->OnSpaceMapUpdate.BindUFunction(this, TEXT("SetWidgetSize"));
+	}
+	this->MeteorRouteCanvasSlot = Cast<UCanvasPanelSlot>(MeteorRouteSize->Slot);
+	if (!this->MeteorRouteCanvasSlot)
+	{
+		return ;
+	}
+	this->MeteorRouteCanvasSlot->SetAlignment(FVector2D(0.5f, 0.5f));
+	this->MeteorRouteCanvasSlot->SetAnchors(FAnchors(0.0f, 0.0f));
+	this->MeteorRouteCanvasSlot->SetAlignment(FVector2D(0.5f, 0.5f));
+	if (this->MeteorWidgetSize)
+	{
+		this->MapSidePx__ = this->MeteorWidgetSize->GetWidthOverride();
+		if (this->MeteorWidgetSize->GetHeightOverride() < this->MapSidePx__)
+		{
+			this->MapSidePx__ = this->MeteorWidgetSize->GetHeightOverride();
+		}
+		this->MapCenter2D__ = FVector2D(this->MapSidePx__ * 0.5f, this->MapSidePx__ * 0.5f);
+	}
+}
+
+void UMeteorEventUserWidget::OnMeteorDetect__(const FMeteor& InMeteor)
+{
+	USpaceSalvageWorldSubsystem* SpaceSubsystem = GetWorld()->GetSubsystem<USpaceSalvageWorldSubsystem>();
+	if (IsValid(SpaceSubsystem))
+	{
+		this->SetWidgetSize(SpaceSubsystem->GetItemSpawnDist());
+	}
+	this->MeteorRouteWidget->SetVisibleToggle(true);
+	this->SetMeteorRoute(InMeteor);
+}
+
+void UMeteorEventUserWidget::OnMeteorClear__()
+{
+	this->MeteorRouteWidget->SetVisibleToggle(false);
+}
