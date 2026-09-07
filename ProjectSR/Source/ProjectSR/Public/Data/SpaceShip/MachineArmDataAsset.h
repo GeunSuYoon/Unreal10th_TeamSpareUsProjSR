d7116930c627955e7fce26e963c1921094afb489
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-07 17:03:48 +0900

    refact: SpaceShip 및 내부 컴포넌트의 데이터를 struct화
    나중에 바꿔야할 때 하나하나 수정하지 않고 바로 할당할 수 있게 리펙토링

--- ProjectSR/Source/ProjectSR/Public/Data/SpaceShip/MachineArmDataAsset.h
+++ ProjectSR/Source/ProjectSR/Public/Data/SpaceShip/MachineArmDataAsset.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CommonHeader/SpaceShipStruct.h"
+
 #include "Engine/DataAsset.h"
 #include "MachineArmDataAsset.generated.h"
 
@@ -16,15 +18,7 @@ class PROJECTSR_API UMachineArmDataAsset : public UPrimaryDataAsset
 	
 public:
 	// 우주 쓰레기 탐사 시 아이템을 수집하는 시간
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MachineArm")
-	float	ItemCollectTime;
-
-	// 우주 쓰레기 탐사 시 수거 가능한 아이템의 최대 무게
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MachineArm")
-	float	ItemCollectWeight;
-
-	// 하루마다 우주선 에너지 소모량. 따로 사용하지 않더라도 하루에 우주선이 소모하는 에너지에 이 값을 추가한다.
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MachineArm|Energy")
-	float	OperationalEnergy;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FMachineArmStat	MachineArmStat;
 
 };
