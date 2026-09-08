b3edd285533104dcf2224e44b3f07f773b166ea7
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-08 15:22:42 +0900

    feat: MainUserWidget에 IWidgetStackHostInterface 상속
    ESC 버튼 등으로 위젯을 끌 때 가장 최초로 열린 OpenableWidget부터 끌 수 있도록 하기 위해 상속함

--- ProjectSR/Source/ProjectSR/Private/Widget/MainUserWidget.cpp
+++ ProjectSR/Source/ProjectSR/Private/Widget/MainUserWidget.cpp
@@ -3,10 +3,12 @@
 
 #include "Widget/MainUserWidget.h"
 #include "Widget/MeteorWarningUserWidget.h"
-#include "SpaceShip/SpaceShipActor.h"
 #include "Widget/InventoryWindowWidget.h"
-#include "Interface/InventoryComponentInterface.h"
+#include "Widget/MainPanel/MainPanelUserWidget.h"
+#include "SpaceShip/SpaceShipActor.h"
 #include "Player/PlayerCharacter.h"
+#include "Interface/InventoryComponentInterface.h"
+#include "Interface/OpenableWidgetInterface.h"
 
 void UMainUserWidget::BindToPlayer(APlayerCharacter* InPlayerCharacter)
 {
@@ -36,8 +38,27 @@ void UMainUserWidget::BindToSpaceShip(ASpaceShipActor* InSpaceShipActor)
 	}
 	this->MeteoWarningWidget->SetVisibility(ESlateVisibility::Collapsed);
 	this->MeteoWarningWidget->BindToMeteorAvoidanceComponent(InSpaceShipActor->GetMeteorAvoidance());
+	this->MainPanelWidget->BindToSpaceShip(InSpaceShipActor);
+	InSpaceShipActor->UpdateSpaceShipLevel();
 }
 
 void UMainUserWidget::BindToCharacter(ACharacter* InCharacter)
 {
+}
+
+bool UMainUserWidget::CloseTopWidget_Implementation()
+{
+	if (this->OpenWidgetStack__.Num() == 0)
+	{
+		return (true);
+	}
+	IOpenableWidgetInterface::Execute_CloseSelfWidget(this->OpenWidgetStack__.Last());
+	this->OpenWidgetStack__.Pop();
+	return (false);
+}
+
+void UMainUserWidget::ClearStackWidget_Implementation()
+{
+	while (!IWidgetStackHostInterface::Execute_CloseTopWidget(this))
+	{ }
 }
