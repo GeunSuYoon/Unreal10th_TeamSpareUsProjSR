ba1b4918910de6e54b2e77b3a1796cc9ac160ccc
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-09 12:21:30 +0900

    feat: 사용할 위젯 c++ 클래스 선언

---
+++ ProjectSR/Source/ProjectSR/Public/Widget/MainWidget/PlayerStat/PlayerHPBarUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerHPBarUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSR_API UPlayerHPBarUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ ProjectSR/Source/ProjectSR/Public/Widget/MainWidget/PlayerStatUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerStatUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSR_API UPlayerStatUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
