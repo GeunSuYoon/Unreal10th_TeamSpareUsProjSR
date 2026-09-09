d2fa023e49755f4ed87c211dd04b2eea98b7cdb9
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-09-09 12:21:30 +0900

    feat: 사용할 위젯 c++ 클래스 선언

---
+++ ProjectSR/Source/ProjectSR/Public/Widget/MainWidget/InventoryAlarmUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryAlarmUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSR_API UInventoryAlarmUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ ProjectSR/Source/ProjectSR/Public/Widget/MainWidget/SpaceShipStat/SpaceShipEnergyUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SpaceShipEnergyUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSR_API USpaceShipEnergyUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
